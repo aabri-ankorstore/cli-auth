be223b48a63b267d3a51a9f90a70be967657262c
Amine AB <amine.abri@ankorstore.com>
2022-09-20 14:11:10 +0100

    fixes

--- utils/variables.go
+++ utils/variables.go
@@ -22,8 +22,3 @@ var (
 	PluginRepo   = "https://github.com/ankorstore/ankor-auth-plugin"
 	PluginPath   = plugin.Encode(PluginRepo)
 )
-
-var KeyPair = [][]byte{
-	[]byte("ePAPW9vJv7gHoftvQTyNj5VkWB52mlza"),
-	[]byte("N8SmpJ00aSpepNrKoyYxmAJhwVuKEWZD"),
-}
