ac6b025f4c1ae666c3d0659a381c5ca47c00e953
Amine AB <amine.abri@ankorstore.com>
2022-09-20 14:14:23 +0100

    showing errors

--- utils/helpers.go
+++ utils/helpers.go
@@ -1,12 +1,14 @@
 package utils
 
 import (
+	"github.com/rs/zerolog/log"
 	"net/http"
 )
 
 func IsAuthenticated(r *http.Request) bool {
 	session, err := SessionStore.Get(r, CookieName)
 	if err != nil || session.Values["access_token"] == nil || session.Values["access_token"] == "" {
+		log.Debug().Err(err)
 		return false
 	}
 	return true
