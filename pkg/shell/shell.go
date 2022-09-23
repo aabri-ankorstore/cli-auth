fa2d86985a561559130e4ddc1c5f2fd45efdf7f5
Amine AB <amine.abri@ankorstore.com>
2022-09-23 16:42:40 +0100

    fixes

--- pkg/shell/shell.go
+++ pkg/shell/shell.go
@@ -8,25 +8,22 @@ import (
 	"os"
 )
 
-func NewShell(sink chan error, f func(commandStr string) error) chan error {
+func NewShell(f func(commandStr string) error) error {
 	reader := bufio.NewReader(os.Stdin)
 	for {
 		cyan := color.New(color.FgCyan)
 		boldCyan := cyan.Add(color.Bold)
 		_, err := boldCyan.Print(fmt.Sprintf("➜ %s: ", util.AppName))
 		if err != nil {
-			sink <- err
-			return sink
+			return err
 		}
 		cmdString, err := reader.ReadString('\n')
 		if err != nil {
-			sink <- err
-			return sink
+			return err
 		}
 		err = f(cmdString)
 		if err != nil {
-			sink <- err
-			return sink
+			return err
 		}
 	}
 }
